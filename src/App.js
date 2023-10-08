54c9b28a5ba2dd1382b8119495a2627f7738877b
Obokhuai <ehitheo@gmail.com>
2023-10-08 13:00:23 +0200

    product screen implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-<>
+    <>
       <Header />
       <main className="py-3">
         <Container>
-        <HomeScreen/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
- 
+      <Footer />
     </>
   );
 };
